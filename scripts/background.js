0500156268b57ce2f800ef5e10507e9e40da112a
pi <90981829+ThePiGuy3141@users.noreply.github.com>
2022-11-30 21:33:53 -0800

    fixed badge

--- scripts/background.js
+++ scripts/background.js
@@ -5,10 +5,10 @@ chrome.webRequest.onBeforeRequest.addListener(
 		if (/.+\/assets\/(?<filename>.+\.js)(?:\?.*)?$/.exec(redirectUrl)) {
 			newRedirectUrl = redirectUrl.replace(/.+\/\/(beta\.)?deeeep.io\/assets/, "https://blockyfish-client.github.io/Hacked-Doc-Assets/images/misc");
 
-			let checkRequest = new XMLHttpRequest(); // creates HTTP request
-			checkRequest.open("GET", newRedirectUrl, false);
-			checkRequest.send();
-			if (checkRequest.status >= 200 && checkRequest.status < 300) {
+			var xhr = new XMLHttpRequest();
+			xhr.open("HEAD", newRedirectUrl, false);
+			xhr.send();
+			if (!xhr.status == "404") {
 				redirectUrl = newRedirectUrl;
 			}
 			return { redirectUrl: redirectUrl };
